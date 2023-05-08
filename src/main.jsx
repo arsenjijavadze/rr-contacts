1bb0a226c03b7a54d7833c75c785af5133e93058
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-05-08 12:20:42 +0300

    feat: configure the route's new action

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Root, {
 import ErrorPage from "./error-page";
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from './routes/contact';
 import EditContact, {
   action as editAction,
@@ -39,6 +40,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
