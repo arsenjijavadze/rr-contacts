43a31dec1c591477010572302caff97635deeec3
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-05-08 11:57:29 +0300

    feat: submitting Forms `onChange`

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,6 +6,7 @@ import {
     Form,
     redirect,
     useNavigation,
+    useSubmit,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -24,6 +25,7 @@ export async function loader({ request }) {
 export default function Root() {
     const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
 
     useEffect(() => {
         document.getElementById("q").value = q;
@@ -35,12 +37,15 @@ export default function Root() {
                 <div>
                     <Form id="search-form" role="search">
                         <input
-                            is="q"
+                            id="q"
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
                             defaultValue={q}
+                            onChange={(event) => {
+                                submit(event.currentTarget.form);
+                            }}
                         />
                         <div
                             id="search-spinner"
@@ -93,7 +98,7 @@ export default function Root() {
             </div>
             <div
                 id="detail"
-                clasName={
+                className={
                     navigation.state === "loading" ? "loading" : ""
                 }
             >
